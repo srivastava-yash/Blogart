80f13355089741336b1a6b7f374b1dfdb5d814da
srivastava-yash <yash.184040@gmail.com>
2024-01-09 17:36:03 -0500

    Article nav ui

--- blogart/src/Components/ReaderNav.js
+++ blogart/src/Components/ReaderNav.js
@@ -3,12 +3,16 @@ import { HiOutlineHome } from "react-icons/hi";
 import { BiBookmarks } from "react-icons/bi";
 import { RiArticleLine } from "react-icons/ri";
 import { BsPencilSquare } from "react-icons/bs";
+import { FiBell } from "react-icons/fi";
 import Image from "next/image";
 
 const styles = {
   wrapper: `w-screen h-[5rem] flex justify-between items-center p[1rem] bg-white`,
   logoContainer: `cursor-pointer`,
   iconContainer: `flex-1 flex justify-center gap-[1.4rem] text-2xl text-black cursor-pointer`,
+  divider: `border-r h-6`,
+  profileImageContainer: `w-[2.4rem] h-[2.4rem] rounded-full overflow-hidden place-items-center`,
+  profileImage: `object-cover`,
 };
 
 const ReaderNav = () => {
@@ -19,11 +23,20 @@ const ReaderNav = () => {
       </div>
       <div className={styles.iconContainer}>
         <HiOutlineHome />
+        <FiBell />
         <BiBookmarks />
         <RiArticleLine />
-        <div className={styles.divider}>
-          <BsPencilSquare />
-        </div>
+        <div className={styles.divider} />
+        <BsPencilSquare />
+      </div>
+
+      <div className={styles.profileImageContainer}>
+        <Image
+          className={styles.profileImage}
+          src={logo}
+          height={50}
+          width={50}
+        />
       </div>
     </div>
   );
